df414d162c950cfc500f1dcb8b483789b56458b2
wang jie <wj0406520@gmail.com>
2018-03-15 18:16:07 +0800

    百练1002

--- main.c
+++ main.c
@@ -1,99 +1,108 @@
 #include <stdio.h>
 #include <string.h>
 
-#define ID 1007
+#define ID 1017
 
 int main()
 {
-	int num,length;
-	scanf("%d",&length);
-	scanf("%d",&num);
-	//多一个存储\n不然变成一行了
-	char str[num][length+1];
-	int count[num];
-	// 按逆序对数从少到多输出字符串，逆序对数一样多的字符串按照输入的顺序输出。
-	// ACGT
-	for (int i = 0; i < num; ++i)
-	{
-		int n=0;
-		int m[4]={0,0,0,0};
-		scanf("%s",str[i]);
-		// printf("%s\n", str[i]);
-		for (int j = 0; j < length; ++j)
-		{
+	int n[6],s[6];
+	int count=10;
 
-			switch(str[i][j]) {
-			   case 'A':
-			   		m[0]+=1;
-			      break;
-			   case 'C':
-			   		n+=length-j-1-m[1];
-			   		m[1]+=1;
-			      break;
-			   case 'G':
-			   		n+=length-j-1-m[2]-m[1];
-			   		m[2]+=1;
-			      break;
-			   case 'T':
-			   		n+=length-j-1-m[3]-m[2]-m[1];
-			   		m[3]+=1;
-			      break;
-			}
+	// for (int i = 0; i < 50; ++i)
+	// {
+	// 	printf("%d %d %d %d %d %d\n", i,i+1,i+2,i+3,i+4,i+5);
+	// 	printf("%d %d %d %d %d %d\n", i+5,i+4,i+3,i+2,i+1,i);
+	// 	/* code */
+	// }
+	// return 0;
+
+	while(1){
+		count = 0;
+		for (int i = 0; i < 6; ++i)
+		{
+			scanf("%d",&n[i]);
+			s[i]=n[i];
 			/* code */
 		}
-		count[i] = n;
+		if(n[0]==0 && n[1]==0 && n[2]==0 && n[3]==0 && n[4]==0 && n[5]==0) return 0;
 
-	}
+		count += n[5];
 
-	int m[2];
-	for (int i = 0; i < num; ++i)
-	{
-		m[0] = count[i];
-		m[1] = i;
-		for (int j = i+1; j < num; ++j)
-		{
-			if(m[0]>count[j]){
-				m[0]=count[j];
-				m[1]=j;
-			}
-			/* code */
+		count += n[4];
+		n[0] -= n[4]*11;
+
+		count += n[3];
+		if(n[1]<5*n[3]){
+			n[0] -= 20*n[3] - n[1]*4;
+			n[1] = 0;
+		}else{
+			n[1] -= n[3]*5;
+			// n[0] -= n[3]*2;
 		}
-		// printf("%d\n", m[1]);
-		// printf("%d\n", count[m[1]]);
-		int f=count[m[1]];
-		count[m[1]]=count[i];
-		count[i]=f;
+		// printf("\n%d %d\n\n", n[0],n[1]);
+		count += n[2]/4;
+		int y = n[2]%4;
 
-		printf("%s\n", str[m[1]]);
-		char *s=str[m[1]];
-		strcpy(str[m[1]],str[i]);
-		strcpy(str[i],s);
-		// printf("%d\n", count[i]);
-		// printf("%s\n", str[i]);
-		/* code */
+		if(y!=0){
+			switch (y){
+				case 1:
+					if(n[1]>5){
+						n[1] -= 5;
+						n[0] -= 7;
+					}else{
+						n[0] -= 36-9-4*n[1];
+						n[1] = 0;
+					}
+				break;
+				case 2:
+					if(n[1]>3){
+						n[1] -= 3;
+						n[0] -= 6;
+					}else{
+						n[0] -= 36-18-4*n[1];
+						n[1] = 0;
+					}
+				break;
+				case 3:
+					if(n[1]>1){
+						n[1] -= 1;
+						n[0] -= 5;
+					}else{
+						n[0] -= 36-27-4*n[1];
+						n[1] = 0;
+					}
+				break;
+			}
+			count++;
+		}
+		// count = -18/2;
+		// printf("%d\n", count);
+		if(n[1]>0){
+			count += n[1]/9;
+			y = n[1]%9;
+			if(y!=0){
+				n[0] -= 36 - 4*y;
+				count++;
+			}
+		}
+
+		if(n[0]>0){
+			count += n[0]/36;
+			y = n[0]%36;
+			if(y!=0){
+				count ++;
+			}
+		}
+		// if(count==3){
+			// printf("%d %d %d %d %d %d\n", s[0],s[1],s[2],s[3],s[4],s[5]);
+		// }else{
+		// 	continue;
+		// }
+		printf("%d\n", count);
+// 5
+// 1 20 5 1 1 1
+// 1+1+1+1+1
+		// printf("%d %d %d %d %d %d\n", n[0],n[1],n[2],n[3],n[4],n[5]);
 	}
-	// 解法1
-	// 1.4后面小于4的都是逆序对
-	// 2.3后面小于3的都是逆序对
-	// 4.2后面小于2的都是逆序对
-
-	// CCCGGGGGGA
-	// AACATGAAGG
-	// GATCAGATTT
-	// ATCGATGCAT
-	// TTTTGGCCAA
-	// TTTGGCCAAA
-	// 解法2
-	// 1.当前是4则有length-1个
-	// 2.当后面有一个则逆序对减1
-	// 3.当前是3则有length-1个
-
-	// 4假设后面都是3则有length-j-1 出现一个4则+1
-	// 3假设后面都是2则有length-j-1 出现一个3或者4则加1
-	// 2假设后面都是1则有length-j-1 出现一个2或者3或者4则加1
-
-	// 2114 3-1=2
-	// 32134  4+3+0-1-1+1-1-1-1=
-
 	return 0;
 }
